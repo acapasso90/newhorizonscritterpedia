a9fc3951afdd09a74f5d24e2795874ae5aa1fa53
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-12-14 16:18:55 -0500

    Updated fishinfoshown and displayfish to be able to display multiple fish
    Updated fishinfoshown and displayfish to be able to display multiple fish. moved previous fishInfo into FishInfoShown and set new fishInfo to response.data

--- src/DisplayFish.js
+++ src/DisplayFish.js
@@ -4,40 +4,26 @@ import FishInfoShown from "./FishInfoShown.js";
 
 export default function DisplayFish(){
     const [fishInfo, setfishInfo] = useState({ready:false});
+    const [ready, setReady] = useState("");
 function showFish(response){
-    const english = "name-USen";
-    const cj = "-cj";
-    const phrase = "catch-phrase";
-    const northernArray = "month-array-northern";
-    const southernArray = "month-array-southern";
-    const timeArray = "time-array";
-setfishInfo({ fishName: response.data[0].name[english],
-    image: <img src= {response.data[0].icon_uri} alt="fishIcon" />,
-    priceNormal: response.data[0].price,
-    priceCJ: response.data[0].price[cj],
-    catchphrase : response.data[0][phrase],
-    availabilityNorth : response.data[0].availability[northernArray],
-    availabilitySouth: response.data[0].availability[southernArray],
-    time: response.data[0].availability[timeArray],
-    allDay: response.data[0].availability.isAllDay,
-    allYear: response.data[0].availability.isAllYear,
-    ready: true,
-
-})
-console.log(response.data);
+setfishInfo(response.data)
+setReady(true);
 }
 function getFish(){
     let apiURL = "https://acnhapi.com/v1a/fish/";
     axios.get(apiURL).then(showFish);
 }
-if (fishInfo.ready){return(
+if (ready){return(
     <div className="getFishInfo">
         <div className="buttons">
             <button  onClick={getFish}>Northern</button>
             <button onClick={getFish}>Southern</button>
         </div>
-        <FishInfoShown data={fishInfo} />
-
+        <FishInfoShown data={fishInfo[0]} />
+        <FishInfoShown data={fishInfo[1]} />
+        <FishInfoShown data={fishInfo[2]} />
+        <FishInfoShown data={fishInfo[4]} />
+        <FishInfoShown data={fishInfo[5]} />
      </div>
     )
 }
--- src/FishInfoShown.js
+++ src/FishInfoShown.js
@@ -1,9 +1,28 @@
 import React from "react";
 
 export default function FishInfoShown(props){
+    const english = "name-USen";
+    const cj = "-cj";
+    const phrase = "catch-phrase";
+    const northernArray = "month-array-northern";
+    const southernArray = "month-array-southern";
+    const timeArray = "time-array";
+    let fishName = props.data.name[english];
+       let image = props.data.icon_uri;
+        let priceNormal = props.data.price;
+       let  priceCJ = props.data.price[cj];
+      let  catchphrase = props.data[phrase];
+       let availabilityNorth = props.data.availability[northernArray];
+        let availabilitySouth = props.data.availability[southernArray];
+        let time = props.data.availability[timeArray];
+        let allDay = props.data.availability.isAllDay;
+        let allYear = props.data.availability.isAllYear;    
+    
 return(
     <div className="FishInfo">
-        <div className="songImage">{props.data.image}</div>
+        <div className="songImage"> 
+        <img src={image} alt="fish icon" />
+    </div>
     </div>
 )
 }
