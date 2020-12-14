35f3dbc173361d312b7301c4feb7cefbc566eba9
acapasso <69538713+acapasso90@users.noreply.github.com>
2020-12-14 14:08:47 -0500

    Added Header.js, Changed background color of body and font-color
    Added Header.js, Changed background color of body and font-color to match game's encycolpedia UI

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div className="Header">
+            <h1>New Horizon's Fish App</h1>
+        </div>
+        )
+}
